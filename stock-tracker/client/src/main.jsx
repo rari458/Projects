4ec0116a9392eed2273dae2a13f55c752e135f8e
rari458 <anstkdcjf1004@gmail.com>
2025-11-07 19:40:44 +0900

    <BrowserRouter> -> <HashRouter>

--- stock-tracker/client/src/main.jsx
+++ stock-tracker/client/src/main.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './api/axiosConfig.js';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
@@ -10,9 +10,9 @@ import { AuthProvider } from './context/AuthContext.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   </React.StrictMode>
 );
